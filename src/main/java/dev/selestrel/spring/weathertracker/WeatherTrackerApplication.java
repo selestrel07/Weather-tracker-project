e0e5d2f793480f1025fb478db77a3fea8f82a2f5
selestrel07 <selestrel07@gmail.com>
2024-01-12 12:20:47 +0400

    project initialization

---
+++ src/main/java/dev/selestrel/spring/weathertracker/WeatherTrackerApplication.java
@@ -0,0 +1,13 @@
+package dev.selestrel.spring.weathertracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherTrackerApplication.class, args);
+    }
+
+}
