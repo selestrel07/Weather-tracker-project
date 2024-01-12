a7753de3e2deee1c60c070fb26e5e92fa22440fb
selestrel07 <selestrel07@gmail.com>
2024-01-12 13:13:38 +0400

    add dtos

---
+++ src/main/java/dev/selestrel/spring/weathertracker/dto/SensorDTO.java
@@ -0,0 +1,13 @@
+package dev.selestrel.spring.weathertracker.dto;
+
+public class SensorDTO {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
